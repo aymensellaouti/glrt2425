eaac9c5808403da8b5ed9a71a838c9fbbb432d8a
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-24 15:58:39 +0100

    exemple js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const t = [1, 2, 3, 4, 11, 21, 23, 45, 35];
+t.sort((a, b) => -a + b);
+console.log(t);
+
+/* t.forEach((value, index, tabKamel) => {
+  console.log(` t[${index}] =  ${value} \n`);
+}), ;
+ */
