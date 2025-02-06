0000cd5099e456373e182ed19bfd7ca7841cd28a
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-06 18:18:13 +0100

    seance du 6 2 2025

--- js/index.js
+++ js/index.js
@@ -6,3 +6,44 @@ console.log(t);
   console.log(` t[${index}] =  ${value} \n`);
 }), ;
  */
+
+// compte à rebours
+
+// index = 5;
+// const intervalId = setInterval(() => {
+//   if (index) {
+//     console.log(index--);
+//   } else {
+//     clearInterval(intervalId);
+//   }
+// }, 2000);
+cities = [
+  "Djerba",
+  "Tunis",
+  "Sfax",
+  "Sousse",
+  "Bizerte",
+  "Nabeul",
+  "Kasserine",
+  "Monastir",
+];
+// const lampe = document.querySelector(".lampe");
+// setInterval(() => {
+//   // lampe.classList.toggle("cc");
+//   lampe.classList.toggle("on");
+//   lampe.classList.toggle("off");
+// }, 2000);
+function createLiCity(cityName) {
+  const liCity = document.createElement("li");
+  liCity.innerHTML = cityName;
+  return liCity;
+}
+// const citiesOl = document.querySelector(".cities");
+// let index = 0;
+// setInterval(() => {
+//   if (!(index % cities.length)) {
+//     citiesOl.innerHTML = "";
+//   }
+//   citiesOl.appendChild(createLiCity(cities[index % cities.length]));
+//   index++;
+// }, 2000);
