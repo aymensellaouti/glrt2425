9f74a5851aacde70c7579d39a78d4fae9764a391
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-06 16:21:05 +0100

    seance du 6 2 2025

--- js/index.js
+++ js/index.js
@@ -1,8 +1,50 @@
-const t = [1, 2, 3, 4, 11, 21, 23, 45, 35];
-t.sort((a, b) => -a + b);
-console.log(t);
+// const t = [1, 2, 3, 4, 11, 21, 23, 45, 35];
+// t.sort((a, b) => -a + b);
+// console.log(t);
 
 /* t.forEach((value, index, tabKamel) => {
   console.log(` t[${index}] =  ${value} \n`);
 }), ;
  */
+// let start = 5;
+// const countdown = setInterval(() => {
+//   if (start) {
+//     console.log(start--);
+//   } else {
+//     clearInterval(countdown);
+//   }
+// }, 1000);
+
+// const ampoule = document.querySelector(".ampoule");
+
+// setInterval(() => {
+//   ampoule.classList.toggle("on");
+//   ampoule.classList.toggle("off");
+// }, 2000);
+
+const cities = [
+  "Djerba",
+  "Tounes",
+  "Hammamet",
+  "Soussa",
+  "Kasserine",
+  "Ariana",
+  "Kairouan",
+];
+
+const cityOl = document.querySelector(".cities");
+
+function createCity(cityName) {
+  const newCity = document.createElement("li");
+  newCity.innerHTML = cityName;
+  return newCity;
+}
+let index = 0;
+// setInterval(() => {
+//   if (!(index % cities.length)) {
+//     cityOl.innerHTML = "";
+//   }
+//   let newCityNode = createCity(cities[index % cities.length]);
+//   cityOl.appendChild(newCityNode);
+//   index++;
+// }, 2000);
