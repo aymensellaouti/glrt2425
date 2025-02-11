5f6d547b82f2cbead61aaee6a8d406169b6a92f8
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-11 15:53:49 +0100

    seance du 7 2 2025

--- js/index.js
+++ js/index.js
@@ -6,3 +6,46 @@ console.log(t);
   console.log(` t[${index}] =  ${value} \n`);
 }), ;
  */
+
+function test(p1, p2) {
+  /// code
+}
+
+function countdown(counter = 5, timer = 1500) {
+  const indexSetInterval = setInterval(() => {
+    if (counter) {
+      console.log(counter--);
+    } else {
+      clearInterval(indexSetInterval);
+    }
+  }, timer);
+}
+// countdown();
+// Je récupére l'élément à manipuler
+const ampoule = document.querySelector(".ampoule");
+// setInterval(() => {
+//   ampoule.classList.toggle("off");
+//   ampoule.classList.toggle("on");
+// }, 1500);
+
+const cities = [
+  "djerba",
+  "kasserine",
+  "sfax",
+  "tunis",
+  "tataouin",
+  "bizerte",
+  "kef",
+];
+let index = 0;
+const citiesOl = document.querySelector(".cities");
+// setInterval(() => {
+//   if (index == cities.length) {
+//     index = 0;
+//   }
+//   if (index == 0) {
+//     citiesOl.innerHTML = "";
+//   }
+//   const li = `<li>${cities[index++]}</li>`;
+//   citiesOl.innerHTML += li;
+// }, 1500);
